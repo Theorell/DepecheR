388a27d78bd5ff4dd73363890901224763b214e3
Jakob Theorell <jakob.theorell@ki.se>
2018-05-02 12:41:23 +0200

    In this commit, the colors of the heat map illustrating the expression at the center of each cluster in each dimension is changed, so that it correctly displays only the sparsed-away areas as grey.

--- R/dClustCoFunction.R
+++ R/dClustCoFunction.R
@@ -56,10 +56,10 @@ dClustCoFunction <- function(inDataFrameScaled, firstClusterNumber=1, penalties=
   #Here, a heatmap over the cluster centers is saved. Only true if the number of clusters exceeds one.
   reducedClusterCentersColRow <- dClustResult[[2]]
   if(nrow(reducedClusterCentersColRow)>1 && ncol(reducedClusterCentersColRow)>1){
+    reducedClusterCentersColRow[reducedClusterCentersColRow==0] <- NA
     colorLadder <- colorRampPalette(c("blue", "white", "red"))(21)
-    colorLadder[11] <- "#A2A2A2"
     pdf("Cluster centers.pdf")
-    heatmap.2(as.matrix(reducedClusterCentersColRow), col=colorLadder, trace="none")
+    heatmap.2(as.matrix(reducedClusterCentersColRow),Rowv=FALSE, Colv=FALSE, dendrogram="none", scale="none", col=colorLadder, trace="none", na.color="#A2A2A2")
     dev.off()    
   }
   
