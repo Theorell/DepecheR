4bfede2ec4a8f56b6c3873d052eb7b5d25b31c72
Axel Theorell <a.theorell@fz-juelich.de>
2018-09-27 14:05:35 +0200

    New tests working, plotting scaling and sPLSDA tests still fail

--- tests/testthat/test_dAllocate.R
+++ tests/testthat/test_dAllocate.R
@@ -5,9 +5,7 @@ context("dAllocate")
 cols <- 150
 x<- DepecheR:::generateBimodalData(observations = 20,dataCols = cols)
 centers<-as.data.frame(rbind(runif(cols)+50,runif(cols)-50))
-colnames(centers) <- paste('V',1:cols, sep="")
-colnames(centers)
-out <-DepecheR:::dAllocate(as.data.frame(x$samples), out$clusterCenters, ids=x$ids)
+out <-DepecheR:::dAllocate(as.data.frame(x$samples), centers, ids=x$ids)
 test_that("dAllocate expected output", {
   expect_true(all(out$realloClusterVector==x$ids) )
 })
--- tests/testthat/test_depeche.R
+++ tests/testthat/test_depeche.R
@@ -6,5 +6,5 @@ x<- DepecheR:::generateBimodalData(observations = 20,dataCols = 150)
 out <- depeche(x$samples, maxIter=8)
 test_that("depeche expected output", {
   expect_true(max(out$clusterVector)==2)
-  expect_true(all(testVec==out$clusterVector) ||all(x$ids %% 2 +1 ==out$clusterVector))
+  expect_true(all(x$ids ==out$clusterVector) ||all(x$ids %% 2 +1 ==out$clusterVector))
 })
