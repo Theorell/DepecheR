8429f06a6adb5ca3c80377c60764f5c8a16faaf0
Axel Theorell <a.theorell@fz-juelich.de>
2018-09-27 13:56:16 +0200

    Changed some tests

--- tests/testthat/test_dAllocate.R
+++ tests/testthat/test_dAllocate.R
@@ -2,32 +2,12 @@
 
 #IDEA: TAKE A DATASET THAT WE ALLOCATE CORRECTLY TO 99%, THEN CHECK THAT THIS HAPPENS WITH THE RAND INDEX!!!
 context("dAllocate")
-
-data(testData)
-testDataSample <- sample(1:nrow(testData), size=48500)
-testDataTrain <- testData[testDataSample,]
-idsTrain <- testData[testDataSample,]
-testDataTest <- testData[-testDataSample,]
-
-setwd("~/Desktop")
-x_depeche_train <- depeche(testDataTrain[,2:15], maxIter=20, sampleSizes=1000, ids=testDataTrain$ids)
-
-
-
-inDataFramePreScaled <- DepecheR:::dScale(testDataTest[,2:15], scale=FALSE, center="peak")
-#Here, all the data is divided by the standard deviation of the full dataset
-sdInDataFramePreScaled <- sd(as.matrix(inDataFramePreScaled))
-inDataFrameScaled <- inDataFramePreScaled/sdInDataFramePreScaled
-
-x_depeche_test <- DepecheR:::dAllocate(inDataFrameScaled, clusterCenters=x_depeche_train$clusterCenters, ids=testDataTest$ids)
-
-######################
-#removeEmptyVariablesAndAllocatePoints
-x <- DepecheR:::generateBimodalData()
-x_train <- x[1:5000,2:ncol(x)]
-x_test <- x[5001:10000,2:ncol(x)]
-depecheResult <- depeche(x_train, maxIter=20, sampleSizes=500)
-allocationResult <- DepecheR:::removeEmptyVariablesAndAllocatePoints(x_test, clusterCenters=depecheResult$clusterCenters)
-
-  
-#test_that("dAllocate expected output"{})
+cols <- 150
+x<- DepecheR:::generateBimodalData(observations = 20,dataCols = cols)
+centers<-as.data.frame(rbind(runif(cols)+50,runif(cols)-50))
+colnames(centers) <- paste('V',1:cols, sep="")
+colnames(centers)
+out <-DepecheR:::dAllocate(as.data.frame(x$samples), out$clusterCenters, ids=x$ids)
+test_that("dAllocate expected output", {
+  expect_true(all(out$realloClusterVector==x$ids) )
+})
--- tests/testthat/test_dOptPenalty.R
+++ tests/testthat/test_dOptPenalty.R
@@ -1,6 +1,18 @@
 ######################
 #dOptPenalty
 context('dOptPenatly')
-x <- DepecheR:::generateBimodalData()[,-1]
-x_scaled <- DepecheR:::dScale(x)
-dOptPenaltyResult <- DepecheR:::dOptPenalty(x_scaled, k=30, maxIter=20, bootstrapObservations=500, penalties=c(2,4), makeGraph=TRUE, disableWarnings=TRUE, minARI=0.95)
+x <- DepecheR:::generateBimodalData(observations = 50,dataCols = 150)
+x_scaled <- DepecheR:::dScale(as.data.frame(x$samples))
+dOPR <- DepecheR:::dOptPenalty(x_scaled, k=30, maxIter=10, disableWarnings=TRUE, minARI=0.95)
+
+ARI <- dOPR[[2]][,1]
+nClust <- dOPR[[2]][,2]
+bestPenalty <- dOPR[[1]]$bestPenalty
+rowNamesdOpr <- row.names(dOPR[[2]])
+nClust[which(rowNamesdOpr==bestPenalty)]
+ARI[which(rowNamesdOpr==bestPenalty)]
+
+test_that("dOptPenalty expected output", {
+  expect_equal(nClust[which(rowNamesdOpr==bestPenalty)],2)
+  expect_equal(ARI[which(rowNamesdOpr==bestPenalty)],1)
+})
