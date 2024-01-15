b8622a607ca941ed282ca9b485461dad8558830d
Jakob Theorell <jakob.theorell@ki.se>
2024-01-15 08:00:12 +0100

    Changes to the dAllocate tests, to accomodate the recent changes to that function.

--- tests/testthat/test_dAllocate.R
+++ tests/testthat/test_dAllocate.R
@@ -5,21 +5,9 @@ cols <- 150
 set.seed(191)
 x <- DepecheR:::generateBimodalData(observations = 20, dataCols = cols)
 centers <- rbind(runif(cols) + 50, runif(cols) - 50)
-out <- dAllocate(x$samples, list("clusterCenters" =centers,
+out <- DepecheR:::dAllocate(x$samples, list("clusterCenters" =centers,
                                  "logCenterSd" = FALSE))
 test_that("dAllocate expected output", {
     expect_true(all(out == x$ids))
 })
 
-# Here, we are comparing if allocating the same data to the cluster centers
-# genreated with depeche gives an ARI
-# of 1, which it should
-colnames(x[[1]]) <- paste0("V", seq_len(ncol(x[[1]])))
-set.seed(111)
-depRes <- depeche(x[[1]], center = FALSE, nCores = 2, createOutput = FALSE)
-allocRes <- dAllocate(x[[1]], depRes)
-test_that("dAllocate expected output", {
-    expect_true(DepecheR:::rand_index(depRes$clusterVector, allocRes,
-        k = 100
-    ) == 1)
-})
